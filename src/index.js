dec5ba793a4c335fc7466b524bede955db2a9e16
aoandrade1 <annaoa@hotmail.com>
2021-09-07 21:06:37 -0400

    Added searchbox component, and App.js, CardList.js, and styled background

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import 'tachyons';
-import { robots } from './robots';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <div className = 'background-i'>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-      </div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+ReactDOM.render (
+          <App />,  document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
